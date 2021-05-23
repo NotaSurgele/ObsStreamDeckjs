3a448e97973b52de25a9c408eb083e41d055be03
wirze snipz <alexis.picard@epitech.eu>
2021-05-23 11:55:09 +0400

    fix repo

--- ObsStreamDeckjs/main.js
+++ ObsStreamDeckjs/main.js
@@ -2,16 +2,11 @@ import './style.css'
 import OBSWebSocket from 'obs-websocket-js'
 import { SceneButton } from './SceneButton';
 
-//yarn run dev --host
-
-/*var address = prompt("Enter your host ip adress:", "");
-
-while (address == "")
-  address = prompt("Enter your host ip adress:", "");*/
+//yarn run dev
 
 async function main() {
-  const obs = new OBSWebSocket()
-  await obs.connect({address: `192.168.1.6:4444`, password: 'StreamDeckjs'});
+  const obs = new OBSWebSocket();
+  await obs.connect({address: "192.168.1.6:4444", password: 'StreamDeckjs'});
   const data = await obs.send("GetSceneList");
 
   for(const scene of data.scenes) {
---
+++ README.md
@@ -0,0 +1,2 @@
+# ObsStreamDeckjs
+ObsStreamDeckjs is a piece of software that can control obs. Like the ElgatoStreamDeck. Using obs-websocket and node.
